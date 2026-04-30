a63406558e54267cb3c3508dce693f7649964793
Alyssa F <aiepillios1101@gmail.com>
2026-04-30 15:40:00 -0400

    Working on front end

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,19 @@
 // Needed Tabs: Dice Settings, Profile, Dice roller
 // We will def need vite for User info within the components
 import '../styles.css'
+// Use vite to omit the data from navbar to react elements for pages
 
 function NavBar() {
     return (
-        <div class="navContainer">        
-            <button class="navControls">Dice Roller</button>
-            <button class="navControls">Dice Settings</button>
-            <button class="navControls">Profile</button>
-        </div>
+        <nav class="navContainer">        
+            <div class="navControls">
+                <a href="https://litera-ggh0.onrender.com/#/">
+                Dice Roller
+                </a>
+                </div>
+            <div class="navControls">Dice Settings</div>
+            <div class="navControls">Profile</div>
+        </nav>
     )
 }
 
